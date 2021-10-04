973c8f11a92b4d43ece22381c6fdb67ac2fe68fd
minhchiu <minhch.vn@gmail.com>
2021-10-04 13:36:23 +0700

    [1.0.0] refactoring render clients

--- src/public/js/helpers/post.js
+++ src/public/js/helpers/post.js
@@ -126,22 +126,3 @@ function outputPost(post, container) {
   const html = createPostHtml(post);
   container.insertAdjacentHTML("afterbegin", html);
 }
-
-function getParents(el, parentSelector /* optional */) {
-  // If no parentSelector defined will bubble up all the way to *document*
-  if (parentSelector === undefined) {
-    parentSelector = document;
-  }
-
-  var parents = [];
-  var p = el.parentNode;
-
-  while (p !== parentSelector) {
-    var o = p;
-    parents.push(o);
-    p = o.parentNode;
-  }
-  parents.push(parentSelector); // Push that parentSelector you wanted to stop at
-
-  return parents;
-}
--- src/public/js/posts/loadPosts.js
+++ src/public/js/posts/loadPosts.js
@@ -1,9 +1,10 @@
 const loadPosts = async () => {
   const { posts } = await httpGet(`/api/posts?followingOnly=true`);
-  if(posts.length == 0) return $(".post__container").insertAdjacentHTML(
-    "afterbegin",
-    '<span class="d-block text-center mt-3">Nothing to show</span>'
-  );
+  if (posts.length == 0)
+    return $(".post__container").insertAdjacentHTML(
+      "afterbegin",
+      '<span class="d-block text-center mt-3">Nothing to show</span>'
+    );
   posts.forEach(post => outputPost(post, $(".post__container")));
 
   // activate like of current user
